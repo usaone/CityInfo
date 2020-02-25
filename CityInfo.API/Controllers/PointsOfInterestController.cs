a9610308367bc6a542aeb833cb3ea595a27783f2
usaone <uddipmitra@gmail.com>
2020-02-25 18:21:48 -0500

    Remove ModelState.IsValid to return BadRequest as it is automatically handled by ApiController

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -50,11 +50,6 @@ namespace CityInfo.API.Controllers
         [HttpPost]
         public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreatingDto pointOfInterest)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest();
-            }
-
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
             if (city == null)
             {
