88855207cce86434f38542362fdf244ced81c66a
usaone <uddipmitra@gmail.com>
2020-03-05 15:08:06 -0500

    Add Description to PointOfInterest and spacing

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -12,10 +12,15 @@ namespace CityInfo.API.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
 
         [ForeignKey("CityId")]
         public City City { get; set; }
+
         public int CityId { get; set; }
     }
 }
