a37e00ab644fca23e8418bacda36c164462c92eb
usaone <uddipmitra@gmail.com>
2020-03-05 15:00:55 -0500

    Update database via code

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Contexts;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
 
@@ -23,6 +26,23 @@ namespace CityInfo.API
             {
                 logger.Info("Initializing application...");
                 var host = CreateWebHostBuilder(args).Build();
+
+                using (var scope = host.Services.CreateScope())
+                {
+                    try 
+                    {
+                        var context = scope.ServiceProvider.GetService<CityInfoContext>();
+
+                        // for demo purposes, delete the database & migrate on startup so 
+                        // we can start with a clean slate
+                        context.Database.EnsureDeleted();
+                        context.Database.Migrate();
+                    } 
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex, "An error occurred while migrating the database.");
+                    }
+                }
 
                 // run the web app
                 host.Run();
