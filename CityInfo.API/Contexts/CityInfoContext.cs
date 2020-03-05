4e6ada0d667f10e10c64e923f357f88b64ce71e2
usaone <uddipmitra@gmail.com>
2020-03-05 13:44:41 -0500

    Add dbcontext and dummycontroller to test

---
+++ CityInfo.API/Contexts/CityInfoContext.cs
@@ -0,0 +1,28 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Contexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; }
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        //// One way to provide the connection string to the DbContext is as follows
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Contexts;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -43,6 +45,11 @@ namespace CityInfo.API
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=CityInfoDB;Trusted_Connection=True;";
+            services.AddDbContext<CityInfoContext>(o => 
+            {
+                o.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
